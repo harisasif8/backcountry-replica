1bd0c0c3dd73be2ca4048599c966539974f5ff17
harisasif8 <harisasif222@gmail.com>
2022-05-27 21:42:30 +0500

    set Indentation

--- scripts/App.js
+++ scripts/App.js
@@ -1,84 +1,83 @@
-const USERS = 'user';
-const LoggedInUser = 'loggedInUser'
+const USERS = "user";
+const LoggedInUser = "loggedInUser";
 
 const getUsersFromLocalStorage = () => {
-    const usersFromLS = localStorage.getItem(USERS);
-    return JSON.parse(usersFromLS);
+  const usersFromLS = localStorage.getItem(USERS);
+  return JSON.parse(usersFromLS);
 };
 
 const checkAuth = () => {
-    const loggedInUser = localStorage.getItem(LoggedInUser);
-    const parsedLoggedInUser = JSON.parse(loggedInUser);
-    const { username } = parsedLoggedInUser || {};
-    const { password } = parsedLoggedInUser || {};
-    const users = JSON.parse(localStorage.getItem(USERS)) || [];
+  const loggedInUser = localStorage.getItem(LoggedInUser);
+  const parsedLoggedInUser = JSON.parse(loggedInUser);
+  const { username } = parsedLoggedInUser || {};
+  const { password } = parsedLoggedInUser || {};
+  const users = JSON.parse(localStorage.getItem(USERS)) || [];
 
-    if (users.find((user) => username === user.username && password === user.password)) {
-        window.location.href = "/Users/hasif/Desktop/Haris Intern task/Authentication/pages/dashboard/home.html"; 
-    }
+  if (
+    users.find(
+      (user) => username === user.username && password === user.password
+    )
+  ) {
+    window.location.href =
+      "/Users/hasif/Desktop/Haris Intern task/Authentication/pages/dashboard/home.html";
+  }
 };
 
 checkAuth();
 
-
-
 const getUserData = (fieldName) => {
-    return document.getElementById(fieldName).value
-}
-
+  //Check
+  return document.getElementById(fieldName).value;
+};
 
 const usersFromLS = getUsersFromLocalStorage();
 const users = usersFromLS ? usersFromLS : [];
 
-
 function signup(e) {
-    e.preventDefault();
-    const firstName = getUserData('fname');
-    const lastName = getUserData('lname');
-    const username = getUserData('username');
-    const password = getUserData('pw');
-    const isUserExist = users.find((user) => user.username === username);
-    const repeatedUser = getUserData('checkUsername');
+  e.preventDefault();
+  const firstName = getUserData("fname");
+  const lastName = getUserData("lname");
+  const username = getUserData("username");
+  const password = getUserData("pw");
+  const isUserExist = users.find((user) => user.username === username);
+  const repeatedUser = getUserData("checkUsername");
 
-    if (isUserExist) {
-        // show error user already exist
-        repeatedUser.innerHTML = "This username already exists."
-    } else {
-        const newUser = {
-            username,
-            password,
-            firstName,
-            lastName
-        };
+  if (isUserExist) {
+    // show error user already exist
+    repeatedUser.innerHTML = "This username already exists.";
+  } else {
+    const newUser = {
+      username,
+      password,
+      firstName,
+      lastName,
+    };
 
-        users.push(newUser)
+    users.push(newUser);
 
-        const userJson = JSON.stringify(users);
-        localStorage.setItem(USERS, userJson)
+    const userJson = JSON.stringify(users);
+    localStorage.setItem(USERS, userJson);
 
-        repeatedUser.innerHTML = ''
-    }
+    repeatedUser.innerHTML = "";
+  }
 }
 
 function login(e) {
-    e.preventDefault();
-    const loginUserName = getUserData('loginUsername');
-    const loginPassword = getUserData('loginpw');
-    const checkLoginUsername = document.getElementById('checkLoginUsername')
-    const isUserExist = users.find((user) => user.username === loginUserName && loginPassword === user.password);
+  e.preventDefault();
+  const loginUserName = getUserData("loginUsername");
+  const loginPassword = getUserData("loginpw");
+  const checkLoginUsername = document.getElementById("checkLoginUsername");
+  const isUserExist = users.find(
+    (user) => user.username === loginUserName && loginPassword === user.password
+  );
 
-    if (isUserExist) {
-
-        location.replace('/Users/hasif/Desktop/Haris Intern task/Authentication/pages/dashboard/home.html')
-        const isUserExistJson = JSON.stringify(isUserExist)
-        localStorage.setItem(LoggedInUser, isUserExistJson)
-
-    }
-    else {
-        checkLoginUsername.innerHTML = "Wrong Username or Password"
-    }
+  if (isUserExist) {
+    location.replace(
+      "/Users/hasif/Desktop/Haris Intern task/Authentication/pages/dashboard/home.html"
+    );
+    const isUserExistJson = JSON.stringify(isUserExist);
+    localStorage.setItem(LoggedInUser, isUserExistJson);
+  } else {
+    checkLoginUsername.innerHTML = "Wrong Username or Password";
+  }
 }
-
-
-
-
--- scripts/home.js
+++ scripts/home.js
@@ -1,32 +1,27 @@
-const LoggedInUser = 'loggedInUser'
-const USERS = 'user';
+const LoggedInUser = "loggedInUser";
+const USERS = "user";
 
-
-
-console.log(USERS)
+console.log(USERS);
 
 const checkAuth = () => {
-    const loggedInUser = localStorage.getItem(LoggedInUser);
-    const parsedLoggedInUser = JSON.parse(loggedInUser);
-    const { username } = parsedLoggedInUser || {};
-    const users = JSON.parse(localStorage.getItem(USERS)) || [];
+  const loggedInUser = localStorage.getItem(LoggedInUser);
+  const parsedLoggedInUser = JSON.parse(loggedInUser);
+  const { username } = parsedLoggedInUser || {};
+  const users = JSON.parse(localStorage.getItem(USERS)) || [];
 
-
-    if (users.find((user) => username === user.username)) {
-        const welcomeText = document.getElementById('userGreet');
-        welcomeText.innerHTML = username;
-    } else {
-        location.replace("/Users/hasif/Desktop/Haris Intern task/Authentication/pages/login/login.html")
-    }
-
-
+  if (users.find((user) => username === user.username)) {
+    const welcomeText = document.getElementById("userGreet");
+    welcomeText.innerHTML = username;
+  } else {
+    location.replace(
+      "/Users/hasif/Desktop/Haris Intern task/Authentication/pages/login/login.html"
+    );
+  }
 };
 checkAuth();
 
-
-
 const logout = () => {
-    localStorage.removeItem(LoggedInUser);
-    window.location.href = "/Users/hasif/Desktop/Haris Intern task/Authentication/pages/login/login.html"
-
-}
+  localStorage.removeItem(LoggedInUser);
+  window.location.href =
+    "/Users/hasif/Desktop/Haris Intern task/Authentication/pages/login/login.html";
+};
